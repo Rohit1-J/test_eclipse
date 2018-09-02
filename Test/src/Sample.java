234cdcfeffad408262be4ce4f75a3a3f9d5ddaea
Rohit <rohit.jalagadugula@gmail.com>
2018-09-02 18:11:41 +0530

    Committing practise

--- Test/src/Sample.java
+++ Test/src/Sample.java
@@ -1,8 +1,12 @@
 
 public class Sample {
 
+	static {
+		System.out.println("Hell" + System.currentTimeMillis());
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!!");
+		System.out.println("Hello World!!" + System.currentTimeMillis());
 	}
 
 }
